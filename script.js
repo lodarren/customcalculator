a0ba91101dd20e65487431c00c8032927dde7392
lodarren <darrenclo@hotmail.com>
2024-06-21 21:28:58 -0700

    working draft of having numbers be displayed on screen

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-let numberone = NaN
-let numbertwo = NaN
-let operator = NaN
-let displaynumber = 0
+let numberOne = "";
+let numberTwo = "";
+let operator = null;
+let screen = document.getElementById('screen');
+
+function evaluate() {
+    if (! numberOne || ! numberTwo || ! operator) {
+        screen.innerText = "ERROR";
+    } else {
+        screen.innerText = operator(parseFloat(numberOne), parseFloat(numberTwo));
+    }
+}
+
+function add() {
+    return numberOne + numberTwo;
+}
+
+function subtract() {
+    return numberOne - numberTwo;
+}
+
+function multiply() {
+    return numberOne * numberTwo;
+}
+
+function divide() {
+    if (numberTwo == 0) {
+        return "lol";
+    } else {
+        return numberOne / numberTwo;
+    }  
+}
+
+function divideround() {
+    if (numberTwo == 0) {
+        return "lol";
+    } else {
+        return math.round(numberOne / numberTwo);
+    }  
+}
+
+function clear(e) {
+    numberOne = "";
+    numberTwo = "";
+    operator = null;
+    screen.innerText = "0";
+}
+
+function appendToDisplay(value) {
+    if (! operator) {
+        numberOne += value;
+        screen.innerText = numberOne;
+    } else {
+        numberTwo += value;
+        screen.innerText = numberTwo;
+    }
+}
+
 
 function setup() {
 
     //buttons
-    document.getElementById("AC").addEventListener("click", clear(e))
+    /**
     document.getElementById("negative").addEventListener("click", negative(e))
     document.getElementById("percent").addEventListener("click", percent(e))
     document.getElementById("division").addEventListener("click", division(e))
@@ -15,30 +69,38 @@ function setup() {
     document.getElementById("sin").addEventListener("click", sine(e))
     document.getElementById("cos").addEventListener("click", cosine(e))
     document.getElementById("tan").addEventListener("click", tan(e))
-    document.getElementById("divideround").addEventListener("click", divideround(e))
     document.getElementById("log").addEventListener("click", logb(e))
     document.getElementById("nPr").addEventListener("click", perm(e))
     document.getElementById("nCr").addEventListener("click", choose(e))
     document.getElementById("power").addEventListener("click", power(e))
-    document.getElementById("add").addEventListener("click", add(e))
-    document.getElementById("seven").addEventListener("click", seven(e))
-    document.getElementById("eight").addEventListener("click", eight(e))
-    document.getElementById("nine").addEventListener("click", nine(e))
     document.getElementById("round").addEventListener("click", round(e))
-    document.getElementById("subtract").addEventListener("click", subtract(e))
-    document.getElementById("four").addEventListener("click", four(e))
-    document.getElementById("five").addEventListener("click", five(e))
-    document.getElementById("six").addEventListener("click", six(e))
     document.getElementById("e").addEventListener("click", euler(e))
-    document.getElementById("multiply").addEventListener("click", multiply(e))
-    document.getElementById("one").addEventListener("click", one(e))
-    document.getElementById("two").addEventListener("click", two(e))
-    document.getElementById("three").addEventListener("click", three(e))
-    document.getElementById("pi").addEventListener("click", pi(e))
+    document.getElementById("pi").addEventListener("click", appendToDisplay(math.pi))
     document.getElementById("DEL").addEventListener("click", del(e))
-    document.getElementById("zero").addEventListener("click", zero(e))
     document.getElementById("decimal").addEventListener("click", decimal(e))
-    document.getElementById("equals").addEventListener("click", equals(e))
+    */
+
+    document.getElementById("AC").addEventListener("click", clear());
+    document.getElementById("divideround").addEventListener("click", () => {if (!numberTwo) {screen.innerText = "//"; operator = divideround;}});
+    document.getElementById("plus").addEventListener("click", () => {if (!numberTwo) {screen.innerText = "+"; operator = add;}})
+    document.getElementById("minus").addEventListener("click", () => {if (!numberTwo) {screen.innerText = "-"; operator = subtract;}});
+    document.getElementById("multiply").addEventListener("click", () => {if (!numberTwo) {screen.innerText = "*"; operator = multiply;}});
+    document.getElementById("equals").addEventListener("click", () => {evaluate()});
+
+    document.getElementById("zero").addEventListener("click", () =>  appendToDisplay("0"));
+    document.getElementById("one").addEventListener("click", () => appendToDisplay("1"));
+    document.getElementById("two").addEventListener("click", () => appendToDisplay("2"));
+    document.getElementById("three").addEventListener("click", () => appendToDisplay("3"));
+    document.getElementById("four").addEventListener("click", () =>  appendToDisplay("4"));
+    document.getElementById("five").addEventListener("click", () => appendToDisplay("5"));
+    document.getElementById("six").addEventListener("click", () => appendToDisplay("6"));
+    document.getElementById("seven").addEventListener("click", () => appendToDisplay("7"));
+    document.getElementById("eight").addEventListener("click", () => appendToDisplay("8"));
+    document.getElementById("nine").addEventListener("click", () => appendToDisplay("9"));
+
+    screen.innerText = "0";
 
 
 }
+
+setup();
